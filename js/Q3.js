309216940605cf9bddc9f294189745123147fd5f
yonig15 <yonig15@gmail.com>
2022-10-27 22:54:26 +0300

    fix_ bag

--- js/Q3.js
+++ js/Q3.js
@@ -1,11 +1,17 @@
 let str = "2147483647"; //(2^31)-1
 let str1 = "-2147483648"; //-2^31
 
-function My_atoi(str) {
-  str = str.replace(/[^-0-9]/gi, "");
-  const Newnum = Number.parseInt(str, 10);
+function My_atoi(StrNum) {
+  StrNum = StrNum.replace(/[^-0-9]/gi, "");
+  let Newnum = Number.parseInt(StrNum, 10);
   if (Newnum >= Math.pow(-2, 31) && Newnum <= Math.pow(2, 31) - 1) {
-    console.log(Newnum);
+    return console.log(Newnum);
+  } else if (Newnum <= Math.pow(-2, 31)) {
+    Newnum = Math.pow(-2, 31);
+    return console.log(Newnum);
+  } else {
+    Newnum = Math.pow(2, 31) - 1;
+    return console.log(Newnum);
   }
 }
 
